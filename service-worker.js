a9d8f06c0304c79b43dd4ee5ec4fed8ffb30a4f0
Mathias Zhang <me@zzzsy.top>
2023-08-14 20:31:15 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('push', function(event) {
+    const options = {
+        body: event.data.text(),
+        icon: 'icon.png',
+        badge: 'badge.png'
+    };
+
+    event.waitUntil(
+        self.registration.showNotification('新消息', options)
+    );
+});
